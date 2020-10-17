77071bb976664a8b4ca376857bde2fe8c4a80f85
Henrik Metzger <henrikm@microsoft.com>
2020-10-17 11:07:51 -0700

    Modified devapp startup and ignore .vscode folder

--- test/devapps/main.go
+++ test/devapps/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"os"
+	"flag"
+	"fmt"
 
 	msalgo "github.com/AzureAD/microsoft-authentication-library-for-go/src/msal"
 )
@@ -15,18 +16,33 @@ var authCodeParams *msalgo.AcquireTokenAuthCodeParameters
 var cacheAccessor = &SampleCacheAccessor{"serialized_cache.json"}
 
 func main() {
-	exampleType := os.Args[1]
-	if exampleType == "1" {
+	var flowNumber string
+	flag.StringVar(&flowNumber, "flow", "1", "Specify flow. Default is 1 (DeviceCode).")
+
+	flag.Usage = func() {
+		fmt.Printf("Usage of our Program: \n")
+		fmt.Printf("./go-project -flow flowNumber\n")
+		fmt.Printf("1 : Public Client - Devide Code\n")
+		fmt.Printf("2 : Public Client - Authorization Code\n")
+		fmt.Printf("3 : Public Client - Username/Password\n")
+		fmt.Printf("4 : Confidential Client - Auth Code\n")
+		fmt.Printf("5 : Confidential Client - Client Secret\n")
+		fmt.Printf("6 : Confidential Client - Certificate\n")
+		//flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	if flowNumber == "1" {
 		acquireTokenDeviceCode()
-	} else if exampleType == "2" {
+	} else if flowNumber == "2" {
 		acquireByAuthorizationCodePublic()
-	} else if exampleType == "3" {
+	} else if flowNumber == "3" {
 		acquireByUsernamePasswordPublic()
-	} else if exampleType == "4" {
+	} else if flowNumber == "4" {
 		acquireByAuthorizationCodeConfidential()
-	} else if exampleType == "5" {
+	} else if flowNumber == "5" {
 		acquireTokenClientSecret()
-	} else if exampleType == "6" {
+	} else if flowNumber == "6" {
 		acquireTokenClientCertificate()
 	}
 }
